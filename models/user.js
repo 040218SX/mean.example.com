1c1d1f06dbc8e8962a53c850903c942673a1e43f
Jason Snider <root@jasonsnider.com>
2018-05-07 10:52:13 -0500

    Added user data

--- models/user.js
+++ models/user.js
@@ -19,7 +19,20 @@ var User = new Schema({
     admin: {
         type: Boolean,
         default: false
+    },
+    created:{
+        type: Date,
+        default: Date.now
+    },
+    modified:{
+        type: Date,
+        default: Date.now
     }
+});
+
+User.pre('save', function(next){
+    this.modified = new Date().toISOString();
+    next();
 });
 
 User.plugin(uniqueValidator);
