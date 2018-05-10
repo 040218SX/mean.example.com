c2357e938902fb9c7611e26174a2dfcb9056501d
Jason Snider <root@jasonsnider.com>
2018-05-10 15:19:59 -0500

    AJAX methods for read and edit

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use(function(req, res, next){
 
 //Session based access control
 app.use(function(req,res,next){
-  //return next();
+  return next();
 
   var whitelist = [
     '/',
@@ -114,6 +114,7 @@ app.use(function(req,res,next){
   //Allow access to dynamic end points
   var subs = [
     '/stylesheets/',
+    '/src/',
   ];
 
   for(var sub of subs){
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../../models/user');
+
 var bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
